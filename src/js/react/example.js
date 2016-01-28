33c74ddf5eefbf29e5023ddff6d04ec85020833a
Brian Newsom <brian.newsom@colorado.edu>
2016-01-27 22:22:27 -0700

    Add basic search functionality - needs styling

--- src/js/react/example.js
+++ src/js/react/example.js
@@ -58,17 +58,65 @@ var CheckBox = React.createClass({
   }
 });
 
+var SearchBox = React.createClass({
+  handleChange: function(event) {
+    console.log(event.target.value);
+    this.props.handleSearchChange(event.target.value);
+  },
+
+  render: function() {
+    return(
+      <div>
+        Search (by command or description): <input type="text" onChange={this.handleChange}/>
+      </div>
+    );
+  },
+});
+
 //Parent element
 var CheckBoxes = React.createClass({
+  getInitialState: function(){
+    return {'searchboxValue': ""}
+  },
+
+  handleSearchChange: function(val) {
+    this.setState({
+    searchBoxValue: val,
+    });
+  },
+
+  normalize: function(str){
+    if (str){
+      return str.toLowerCase();
+    } else {
+      return "";
+    }
+  },
+
+  doesInclude: function(cmd, searchVal){
+    if (this.normalize(cmd.description).includes(searchVal) || this.normalize(cmd.command).includes(searchVal))
+      return true;
+    else
+      return false;
+  },
+
   render: function() {
     let checkList = [];
+    var searchVal = this.normalize(this.state.searchBoxValue);
     commands.forEach(function (command, index) {
-      checkList.push(<CheckBox command={command} key={index}/>);
-    });
+      /* Only include if searched for */
+      if (this.doesInclude(command, searchVal)){
+        checkList.push(<CheckBox command={command} key={index}/>);
+      }
+    }.bind(this))
+
     return (
-      <form>
-        {checkList}
-      </form>
+      <div>
+        <SearchBox handleSearchChange={this.handleSearchChange}/>
+        <form>
+          {checkList}
+        </form>
+      </div>
     );
   }
 });
