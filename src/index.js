6937633f557e6c78eb4fa76144c0571beddf3426
Dawson Botsford <DawsonBotsford@gmail.com>
2018-12-13 08:30:07 -0500

    ⚙️  Update and remove serviceWorker logic

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import unregisterServiceWorker from './registerServiceWorker';
-unregisterServiceWorker();
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.unregister();
